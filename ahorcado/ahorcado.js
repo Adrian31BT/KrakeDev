3344ad34ae5061de13bcd49825334a1922b3ed07
Adrian31BT <adrian31baciliot@gmail.com>
2024-08-13 20:44:01 -0500

    Paso 1: implementado funcion guardarPalabra

--- ahorcado/ahorcado.js
+++ ahorcado/ahorcado.js
@@ -1,4 +1,31 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+let palabraSecreta;
+
+guardarPalabra = function(){
+    let palabra;
+    let longitud;
+    let validar = true;
+    palabra = recuperarTexto("txtSecreta");
+    longitud = palabra.length
+    if(longitud == 5){
+        for(let i=0; i<longitud; i++){
+            caracter = palabra.charAt(i);
+            if(!esMayuscula(caracter)){
+                validar = false;
+            }
+        }
+    }else{
+        validar = false;
+    }
+
+    if(validar == false){
+        alert("Ingresar una palabra de 5 letras mayusculas");
+    }else{
+        palabraSecreta = palabra;
+    }
+    console.log(palabraSecreta);
+}
+
 esMayuscula = function(caracter){
     let codigo; 
     codigo = caracter.charCodeAt(0);
@@ -8,3 +35,4 @@ esMayuscula = function(caracter){
         return false;
     }
 }
+
