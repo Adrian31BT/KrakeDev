fd689c05447e002f8b5118eccfafd81d60726694
Adrian31BT <adrian31baciliot@gmail.com>
2024-08-30 18:14:21 -0500

    Parte 1: Agregar persona implementada

---
+++ AdrianBacilio/orden.js
@@ -0,0 +1,34 @@
+let personas = [
+    {nombre:"Marcos", edad: 18},
+    {nombre:"Roberto", edad: 15},
+    {nombre:"Kate", edad: 25},
+    {nombre:"Diana", edad: 12},
+    {nombre:"Benja", edad: 5}, 
+];
+
+agregarPersona = function(){
+    let validar = true;
+    let nombre = recuperarTexto("txtNombre");
+    let edad = recuperarInt("txtEdad");
+
+    let longitudNombre = nombre.length;
+
+    if(longitudNombre < 3){
+        validar = false;
+        mostrarTexto("lblErrorNombre","*Ingrese minimo 3 caracteres");
+    }
+
+    if(edad < 0 || edad >100 || isNaN(edad)){
+        validar = false;
+        mostrarTexto("lblErrorEdad","*Ingrese edad entre 0 - 100");
+    }
+
+    if(validar == true){
+        let nuevaPersona = {};
+        nuevaPersona.nombre = nombre;
+        nuevaPersona.edad = edad;
+        personas.push(nuevaPersona);
+        alert("PERSONA AGREAGDA CORRECTAMENTE");
+        console.log(personas);
+    }
+}
